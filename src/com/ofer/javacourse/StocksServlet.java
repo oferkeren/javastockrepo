c5bfd903747402b46d5ebafb5bf86194fd137382
ofer.keren <ofer.keren@gmail.com>
2014-12-14 13:40:24 +0200

    commit n2

---
+++ src/com/ofer/javacourse/StocksServlet.java
@@ -0,0 +1,11 @@
+package com.ofer.javacourse;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class StocksServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
