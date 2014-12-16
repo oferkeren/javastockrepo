6d11eca97706abcac7999978305b7e00d1a35bcf
ofer.keren <ofer.keren@gmail.com>
2014-12-16 16:25:08 +0200

    ex 2 - math calculation

--- src/com/ofer/javacourse/StocksServlet.java
+++ src/com/ofer/javacourse/StocksServlet.java
@@ -5,7 +5,14 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class StocksServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		//variable declaration
+		int num1=4;
+		int num2=3;
+		int num3=7;
+		int result = (num1+num2)*num3;
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		String resultStr = new String("Result of ("+num1+"+"+num2+")*"+num3+"="+result);
+		resp.getWriter().println(resultStr);
+	//	resp.getWriter().println("Hello, world");
 	}
 }
