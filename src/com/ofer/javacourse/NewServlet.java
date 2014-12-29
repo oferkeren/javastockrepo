2343757bd9ce5491188390a8fc2156227468df00
ofer.keren <ofer.keren@gmail.com>
2014-12-29 13:19:00 +0200

    commit math 3

--- src/com/ofer/javacourse/NewServlet.java
+++ src/com/ofer/javacourse/NewServlet.java
@@ -22,15 +22,15 @@ public class NewServlet extends HttpServlet {
 		int hypo=50;
 		double angleB=30;
 		double radians = Math.toRadians(angleB);
-		double opposite=Math.sin(radians)*hypo;
+		double opposite=(int) Math.ceil(Math.sin(radians)*hypo);
 		
 		double base=20;
 		double exp=13;
 		double power=Math.pow(base, exp);
 		
-		String line1=new String ("Area of circle with radius "+radius+ " is: "+circleArea+" squrae-cm.<br>");
-		String line2=new String ("Length of opposite where angle B is "+angleB+" degrees and Hypotenuse length is "+hypo+"cm is: "+opposite+"cm<br>");
-	    String line3=new String ("Power of "+base+" with exp of "+exp+" is "+power);
+		String line1=new String ("<b>Area of circle with radius "+radius+ " is: "+circleArea+" squrae-cm.<br></b>");
+		String line2=new String ("<b>Length of opposite where angle B is "+angleB+" degrees and Hypotenuse length is "+hypo+"cm is: "+(int)opposite+"cm<br></b>");
+	    String line3=new String ("<b>Power of "+base+" with exp of "+exp+" is "+power+" </b>");
 		
 		
 		
